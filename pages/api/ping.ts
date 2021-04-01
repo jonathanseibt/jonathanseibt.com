6eaea8a3e0c731d3388374e200ad3a725825a4f3
jonathanseibt <jonathanvseibt@gmail.com>
2021-03-31 21:36:55 -0300

    chore: add 'http-status-codes'

--- pages/api/ping.ts
+++ pages/api/ping.ts
@@ -1,5 +1,6 @@
+import { StatusCodes } from 'http-status-codes';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (_req: NextApiRequest, res: NextApiResponse<string>): void => {
-  res.status(200).json('pong');
+  res.status(StatusCodes.OK).json('pong');
 };
