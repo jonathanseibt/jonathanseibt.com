6fc7d2110ea3d45324764a7cfb81e54e4293c18a
jonathanseibt <jonathanvseibt@gmail.com>
2021-04-02 00:04:54 -0300

    feat: add open graph meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,22 @@ const MyApp: FC<AppProps> = ({ Component, pageProps: props }) => {
     <>
       <Head>
         <title>Jonathan Seibt • Senior Front End Engineer</title>
+        <meta name='description' content="Hey! My name is Jonathan Seibt, and I'm a Senior Front End Engineer." />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://jonathanseibt.com/' />
+        <meta property='og:title' content='Jonathan Seibt' />
+        <meta property='og:description' content="Hey! My name is Jonathan Seibt, and I'm a Senior Front End Engineer." />
+        <meta property='og:image' content='https://jonathanseibt.com/jonathan-seibt-preview.png' />
+        <meta property='og:image:secure_url' content='https://jonathanseibt.com/jonathan-seibt-preview.png' />
+        <meta property='og:image:width' content='512' />
+        <meta property='og:image:height' content='512' />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://jonathanseibt.com/' />
+        <meta property='twitter:title' content='Jonathan Seibt' />
+        <meta property='twitter:description' content="Hey! My name is Jonathan Seibt, and I'm a Senior Front End Engineer." />
+        <meta property='twitter:image' content='https://jonathanseibt.com/jonathan-seibt-preview.png' />
 
         <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width' />
 
