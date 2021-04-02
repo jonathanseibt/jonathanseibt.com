4bafa2978fe0b9955435f7eda8239ea337767eb4
jonathanseibt <jonathanvseibt@gmail.com>
2021-04-01 22:31:52 -0300

    chore: remove unused dependencies

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,7 +39,6 @@ export default class MyDocument extends Document {
 
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Alegreya:wght@100;200;300;400;500;600;700;800;900&display=swap' />
-          <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
         </Head>
 
         <body>
