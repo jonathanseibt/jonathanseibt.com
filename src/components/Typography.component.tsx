5f07aa2bc642d962c5dd3fa06dee4b90cf598888
jonathanseibt <jonathanvseibt@gmail.com>
2021-05-05 23:18:23 -0300

    feat: improve typography component

--- src/components/Typography.component.tsx
+++ src/components/Typography.component.tsx
@@ -14,15 +14,16 @@ interface CustomStyleProps {
   fontWeight?: CSS.Property.FontWeight;
   lineHeight?: CSS.Property.LineHeight;
   fontSize?: CSS.Property.FontSize;
+  color?: CSS.Property.Color;
 }
 
-interface Props extends CustomStyleProps, Omit<TypographyProps, 'classes' | 'className' | 'style'>, Pick<BoxProps, 'component'>, Pick<LinkProps, 'href'> {
+interface Props extends CustomStyleProps, Omit<TypographyProps, 'classes' | 'className' | 'style' | 'color'>, Pick<BoxProps, 'component'>, Pick<LinkProps, 'href'> {
   spaceBefore?: boolean;
   spaceAfter?: boolean;
 }
 
-const Typography: FC<Props> = ({ fontWeight, lineHeight, fontSize, spaceBefore, spaceAfter, component, ...rest }) => {
-  const customStyles = { fontWeight, lineHeight, fontSize };
+const Typography: FC<Props> = ({ fontWeight, lineHeight, fontSize, color, spaceBefore, spaceAfter, component, ...rest }) => {
+  const customStyles = { fontWeight, lineHeight, fontSize, color };
   const styles = useStyles(customStyles);
   const hasCustomStyles = !!Object.values(customStyles)?.filter((style) => style)?.length;
 
