5754665db253f6b32ebb1779633309c34db053bb
jonathanseibt <hey@jonathanseibt.com>
2023-10-10 18:21:09 -0300

    chore: bump dependencies & some housekeeping

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function PingApi(_request: NextApiRequest, response: NextApiResponse<string>): void {
   response.status(StatusCodes.OK).json('pong')
